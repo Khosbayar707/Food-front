7c8be70932ffd94d3f50fc2dbc05739efb170d3c
Khosbayar707 <torutora0415@gmail.com>
2025-03-26 17:53:24 +0800

    update

--- src/app/_component/MainSection.tsx
+++ src/app/_component/MainSection.tsx
@@ -4,15 +4,32 @@ import { MainCard } from "./MainCard";
 import { Category } from "../types";
 import { useAuth } from "@clerk/nextjs";
 import { useAuthFetch } from "../useFetchData";
+import { useEffect, useState } from "react";
 
 export function MainSection() {
-  const { isLoading, data: categories } = useAuthFetch("food-category");
-  if (isLoading) return <div>Loading...</div>;
+  const { getToken } = useAuth();
+  const [data, setData] = useState([]);
+  async function getFetchData() {
+    const token = await getToken();
+    if (token) {
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/$food-category`, {
+        method: "GET",
+        headers: {
+          authentication: token,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    }
+  }
+  useEffect(() => {
+    getFetchData();
+  }, []);
 
   return (
     <div>
-      {categories &&
-        categories.map((category: Category) => (
+      {data &&
+        data.map((category: Category) => (
           <div key={category?._id} className="py-2 px-6 rounded-lg font-bold">
             <div className="mx-1 my-2">
               <div className="text-white my-4 w-[90%] mx-auto text-xl hover:text-grey">
