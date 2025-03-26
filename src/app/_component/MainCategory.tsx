21149adfc693a6b8f0b60d16536d75455507ad40
Khosbayar707 <torutora0415@gmail.com>
2025-03-26 17:50:59 +0800

    update

--- src/app/_component/MainCategory.tsx
+++ src/app/_component/MainCategory.tsx
@@ -7,13 +7,31 @@ import { ChevronLeft } from "lucide-react";
 import { ChevronRight } from "lucide-react";
 import { Category } from "../types";
 import { useAuthFetch } from "../useFetchData";
+import { useAuth } from "@clerk/nextjs";
 
 export function MainCategory() {
   // const searchParams = useSearchParams();
   // const category = searchParams.get("category");
 
-  const { isLoading, data: categories } = useAuthFetch("food-category");
-  if (isLoading) return <div>Loading...</div>;
+  // const { isLoading, data: categories } = useAuthFetch("food-category");
+  const { getToken } = useAuth();
+  const [data, setData] = useState([]);
+  async function getFetchData() {
+    const token = await getToken();
+    if (token) {
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/$food-category`, {
+        method: "GET",
+        headers: {
+          authentication: token,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    }
+  }
+  useEffect(() => {
+    getFetchData();
+  }, []);
 
   return (
     <div>
@@ -21,8 +39,8 @@ export function MainCategory() {
         <div className="text-white">
           <ChevronLeft />
         </div>
-        {categories &&
-          categories.map((category: Category) => (
+        {data &&
+          data.map((category: Category) => (
             <Link
               href={`/${category?.categoryName}?category=${category?._id}`}
               key={category?._id}
